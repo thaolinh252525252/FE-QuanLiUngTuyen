d85dce13bea433a972fcc1a3023caa7934446330
unknown <thaolinh252512@gmail.com>
2025-05-21 09:42:40 +0700

    Cập nhật code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/', // Đường dẫn gốc, mặc định là '/'
+  build: {
+    outDir: 'dist', // Thư mục build (mặc định là 'dist')
+  },
+});
